7f31c4e1b3bac1ce3c0cc361471133e65410709b
Bhavya-Dhanawani <bhavyadhanwani1234@gmail.com>
2026-07-16 03:26:13 +0530

    Env setup made

---
+++ server/src/shared/config/env.config.js
@@ -0,0 +1,27 @@
+// Importing modules
+import { config } from "dotenv";
+import z from "zod";
+import envConstants from "../constants/env.constants.js";
+
+// loading environment variables
+config();
+
+// defining the schema for environment variables
+const envSchema = z.object({
+    PORT: z.coerce.number().default(envConstants.PORT),
+    NODE_ENV: z.enum(["development", "production", "test"]).default(envConstants.NODE_ENV),
+    MONGO_URL: z.string().default(envConstants.MONGO_URL),
+});
+
+// parsing and validating environment variables
+const parsedEnv = envSchema.safeParse(process.env);
+
+if (!parsedEnv.success) {
+    console.error("Invalid environment variables:", parsedEnv.error.format());
+    process.exit(1);
+}
+
+// getting the validated environment variables
+const env = parsedEnv.data;
+
+export default env;
